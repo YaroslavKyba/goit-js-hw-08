5e28a0ccea5b29b5702c8ac5b65b65786047d059
YaroslavKyba <narxoc.ne@gmail.com>
2023-11-11 15:32:05 +0200

    third commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,16 @@
+import throttle from 'lodash.throttle';
+
 import Player from '@vimeo/player';
 
-const iframe = document.querySelector('.vimeo-player');
+const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 const CURRENT_TIME = 'videoplayer-current-time';
 
-const currentMoment = player.on('timeupdate', function () {
-  console.log('play the video!');
-  localStorage.setItem(CURRENT_TIME, JSON.stringify());
-});
+player.on('timeupdate', throttle(onData, 1000));
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+
+function onData(data) {
+  localStorage.setItem(CURRENT_TIME, JSON.stringify(data.seconds));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,10 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const KEY_ACC = 'feedback-form-state';
 
-const formData = {};
+const formData = {
+  email: form.elements.email.value,
+  message: form.elements.email.message,
+};
 populateMessageInput();
 
 form.addEventListener('input', throttle(onFormInput, 500));
@@ -17,6 +20,7 @@ function onFormInput(evt) {
 
 function onFormSubmit(evt) {
   evt.preventDefault();
+  console.log(localStorage.getItem(KEY_ACC));
   evt.currentTarget.reset();
 
   localStorage.removeItem(KEY_ACC);
@@ -24,9 +28,11 @@ function onFormSubmit(evt) {
 
 function populateMessageInput() {
   const savedMessage = localStorage.getItem(KEY_ACC);
-  console.log(savedMessage);
-  if (savedMessage) {
+   if (savedMessage) {
     form.elements.email.value = JSON.parse(savedMessage).email;
     form.elements.message.value = JSON.parse(savedMessage).message;
+  } else {
+    form.elements.email.value = '';
+    form.elements.message.value = '';
   }
 }
